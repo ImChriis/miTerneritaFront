2776d7f282f1a10cfdd1c349a76cc6f3b0a76eaf
Chris <christophermedinabarroso@gmail.com>
2026-06-09 16:54:23 -0400

    settings observable

--- src/app/@core/services/settings.service.ts
+++ src/app/@core/services/settings.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.developer';
 import { HttpClient } from '@angular/common/http';
+import { Subject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,16 @@ import { HttpClient } from '@angular/common/http';
 export class SettingsService {
   private api: string = environment.api;
   private http = inject(HttpClient);
+  private refresh$ = new Subject<void>();
+  public refreshObservable$ = this.refresh$.asObservable();
 
   getSettings() {
     return this.http.get(`${this.api}/configuration`);
   }
 
   updateSettings(settings: any) {
-    return this.http.put(`${this.api}/configuration`, settings);
+    return this.http.put(`${this.api}/configuration`, settings).pipe(
+      tap(() => this.refresh$.next())
+    )
   }
 }
