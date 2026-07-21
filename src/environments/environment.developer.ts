54312faf71426c061408f05c42629407b6e40844
Chris <christophermedinabarroso@gmail.com>
2026-07-21 15:24:58 -0400

    img

--- src/environments/environment.developer.ts
+++ src/environments/environment.developer.ts
@@ -3,8 +3,9 @@ export const environment = {
 
     //Development
     // api: 'https://4166zxqp-8000.use2.devtunnels.ms',
-    apiImg: 'https://4166zxqp-8000.use2.devtunnels.ms/assets/img',
+    // apiImg: 'https://4166zxqp-8000.use2.devtunnels.ms/assets/img',
 
     //Production
-    api: 'https://miterneritaback.onrender.com'
+    api: 'https://miterneritaback.onrender.com',
+    apiImg: 'https://miterneritaback.onrender.com/assets/img',
 }
